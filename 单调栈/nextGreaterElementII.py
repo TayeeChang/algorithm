936b56fb206354ca3ddb9443a4ac29b923fab780
zhanghaojie <zhanghaojie@mininglamp.com>
2021-04-24 13:38:27 +0800

    update

--- 单调栈/nextGreaterElementII.py
+++ 单调栈/nextGreaterElementII.py
@@ -4,18 +4,21 @@
 
 
 def nextGreaterElement(nums):
+    """
+    环形数组通常使用的是将数组长度翻倍，然后使用求模运算（%）技巧
+    """
     n = len(nums)
     res = [-1] * n
-    nums += nums
     stack = []
-    for i in range(len(nums)):
-        while stack and nums[stack[-1]] < nums[i]:
+    for i in range(2*n):
+        while stack and nums[stack[-1]] < nums[i % n]:
             index = stack.pop()
-            res[index] = nums[i]
+            res[index] = nums[i % n]
         stack.append(i % n)
     return res
 
 
 nums = [1, 2, 1]
 print(nextGreaterElement(nums))
+
 
