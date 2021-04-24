ebe44d1ade493663125c86a445bc8cca50a94ddc
zhanghaojie <zhanghaojie@mininglamp.com>
2021-04-24 13:23:34 +0800

    add 双指针

--- 双指针/trap.py
+++ 双指针/trap.py
@@ -1,26 +1,8 @@
 # -*- coding: utf-8 -*-
-# @Time : 2021/4/22 15:20
+# @Time : 2021/4/24 13:21
 # @Author : haojie zhang
 
-# 接雨水
 
-
-#单调栈
-def trap(height):
-    stack = []
-    res = 0
-    for i, num in enumerate(height):
-        while stack and stack[-1][1] < num:
-            j, curr = stack.pop()
-            if stack:
-                height = min(stack[-1][1], num) - curr
-                width = i - stack[-1][0] - 1
-                res += height * width
-        stack.append((i, num))
-    return res
-
-
-# 双指针
 def trap_2_points(height):
     if not height:
         return 0
@@ -39,5 +21,5 @@ def trap_2_points(height):
     return res
 
 
-height = [4, 2, 0, 3, 2, 5]
-print(trap(height))
+height = [0,1,0,2,1,0,1,3,2,1,2,1]
+print(trap_2_points(height))
